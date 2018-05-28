d516080b2fb7ca9b996b5d4f1cad40486949ab91
Peter Delmeby <peter.delmeby@gmail.com>
2018-05-28 14:44:58 +0200

    Some refactoring and renaming.

--- antagna/index.js
+++ antagna/index.js
@@ -11,7 +11,7 @@ async function run () {
       name: 'choice',
       choices: [
         {name: 'För att ta bort antagna ur samtliga kurser i Canvas', value: 'createUnenrollmentFile'},
-        {name: 'För att skapa antagna för perioder i Canvas', value: 'createAntagnaEnrollmentFile'},
+        {name: 'För att lägga till antagna för grupper i Canvas', value: 'createSectionsEnrolledFile'}, // ToDo: Perioder best name?
         {name: 'För att skapa upp kurser, sektioner och enrollments i Canvas', value: 'createCoursesSectionsEnrollment'}
       ],
       type: 'list'
@@ -21,9 +21,14 @@ async function run () {
       const createCoursesSectionsEnrollment = require('./createCoursesSectionsEnrollmentsFile.js')
       await createCoursesSectionsEnrollment()
       break
-    case 'createAntagnaEnrollmentFile':
-      const createAntagnaEnrollmentFile = require('./createAntagnaEnrollmentFile.js')
-      await createAntagnaEnrollmentFile()
+    case 'createSectionsEnrolledFile':
+      const createSectionsEnrolledFile = require('./createSectionsEnrolledFile.js') // ToDo: There is already an enrollmentSfile - best new name?
+      try {
+        await createSectionsEnrolledFile()
+      } catch (e) {
+        // ToDo: What to do here?
+        console.log(e)
+      }
       break
     case 'createUnenrollmentFile':
       const createUnenrollmentFile = require('./createUnenrollmentFile.js')
