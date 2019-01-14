40d1d74410c11200cfa8ecc83c60e58a0f80c914
Emil Stenberg <emilsten@kth.se>
2019-01-14 10:59:21 +0100

    added possibility to start from a specific page

--- search-in-sis-imports/index.js
+++ search-in-sis-imports/index.js
@@ -34,8 +34,15 @@ async function search(){
     })
     const re = new RegExp(searchString)
 
+    const {startPage} = await inquirer.prompt({
+        name: 'startPage',
+        message: 'Starta på sida',
+        default: 0,
+        type: 'number'
+    })
+
     const canvasApi = new CanvasApi(apiUrl, apiKey)
-    canvasApi.get('accounts/1/sis_imports', async data =>{
+    canvasApi.get(`accounts/1/sis_imports?per_page=100&page=${startPage}`, async data =>{
         console.log(`Checking sis import from date ${data.sis_imports[0].created_at}`.yellow)
         for (const sis of data.sis_imports) {
             const url = sis.csv_attachments && sis.csv_attachments[0].url 
