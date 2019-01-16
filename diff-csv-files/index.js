3ba09a9bab3f7fd126e239d4636e6b70481c0bf3
Emil Stenberg <emilsten@kth.se>
2019-01-16 13:47:59 +0100

    write diffs

--- diff-csv-files/index.js
+++ diff-csv-files/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+require('colors')
 async function diff(){
     const file1 = 'all-ug-users.csv'
     const file2 = 'provisioning_csv_16_Jan_2019_15720190116-10161-2q8ts0.csv'
@@ -12,6 +13,22 @@ async function diff(){
         const [ sis_id ]= line.split(',') 
         file2contentObj[sis_id] = line
     }
-    console.log( file2contentObj )
+    
+    const file1content = fs.readFileSync(file1, 'utf8')
+        .toString()
+        .split('\n')
+
+    
+    for (const line of file1content){
+        const [ sis_id ]= line.split(',')
+        //console.log(file2content[sis_id])
+        //console.log(sis_id,line)
+        if(line !== file2contentObj[sis_id]){
+            console.log('-----------------')
+            console.log(`>>>     UG: ${line}`.blue)
+            console.log(`<<< Canvas: ${file2contentObj[sis_id]}`.yellow)
+        }
+    }
+
 } 
 diff()
