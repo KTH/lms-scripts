2aee9d3400651bd5e9d252e597724ac34828bd80
Emil Stenberg <emilsten@kth.se>
2019-01-17 14:41:45 +0100

    just print a little more info

--- diff-csv-files/index.js
+++ diff-csv-files/index.js
@@ -7,6 +7,7 @@ async function diff(){
 
     const provisioningFilecontentObj = {}
     const diffingProvisioningFileContentObj= {}
+    const oldProvisioningFileContentObj= {}
 
     const provisioningFilecontent = fs.readFileSync(provisioningFile, 'utf8')
         .toString()
@@ -18,13 +19,20 @@ async function diff(){
             sis_id, login_id,email,created_by_sis
         }
     }
-    
+
     const oldProvisioningFilecontent = fs.readFileSync(oldProvisioningFile, 'utf8')
         .toString()
         .split('\n')
 
     for (const line of oldProvisioningFilecontent) {
         const [ ,sis_id,login_id,email,,created_by_sis  ]= line.split(',') 
+
+        // Först spara alla rader från denna fil som key value
+        oldProvisioningFileContentObj[sis_id] = {
+            sis_id, login_id,email,created_by_sis
+        }
+
+        // Sen: spara de rader som skiljer sig mellan nya o gamla prosisioningfil
         const provisioningObj = provisioningFilecontentObj[sis_id] 
         if( provisioningObj && email !== provisioningObj.email ){            
             diffingProvisioningFileContentObj[sis_id]={
@@ -37,31 +45,39 @@ async function diff(){
         .toString()
         .split('\n')
 
-    let i = 0 
+    let diffingEmailsUgOldProv = 0 
     for (const line of ugFilecontent){
         const [ sis_id, username, email ]= line.split(',')
+        const oldProvObj = oldProvisioningFileContentObj[sis_id]
+
+        /*if(
+            oldProvObj && email &&
+            email !== oldProvObj.email
+        ){
+            diffingEmailsUgOldProv++
+                console.log('-----------------')
+                console.log('sisid: ',sis_id)
+                console.log('Email skiljer sig mellan gamla provisioning och ug: ')
+                console.log(email.blue, oldProvObj.email.yellow)
+        }
+        */
         const provisioningObj = provisioningFilecontentObj[sis_id] 
         const diffingObj = diffingProvisioningFileContentObj[sis_id]
         if(
+
             diffingObj &&
             provisioningObj &&
             email !== provisioningObj.email
         ){
             // Alla users som diffar mellan ug och canvas
-            i++
-            /*console.log('-----------------')
+            //i++
+            console.log('-----------------')
             console.log(`<<< Canvas: ${provisioningObj.email}`.yellow)
             console.log(`>>> Augusti: ${diffingObj.email}`.yellow,)
-            console.log(`>>>     UG: ${email}`.blue)*/
-            if(email && email !== diffingObj.email){
-                console.log('-----------------')
-                console.log('sisid: ',sis_id)
-                console.log('Email skiljer sig mellan gamla provisioning och ug: ')
-                console.log(email.blue, diffingObj.email.yellow)
-            }
+            console.log(`>>>     UG: ${email}`.blue)
         }
     }
-    console.log('Total: ', i)
+    //console.log('Totalt antal användare med olika epost mellan ug och gamla provisioning: ', diffingEmailsUgOldProv)
 
 } 
 diff()
