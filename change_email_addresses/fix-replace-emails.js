faa5f2cac2a1d806e8a03b9b8666b4b47240e8f2
Emil Stenberg <emilsten@kth.se>
2019-01-24 11:22:06 +0100

    double check that users only have one email channel

--- change_email_addresses/fix-replace-emails.js
+++ change_email_addresses/fix-replace-emails.js
@@ -1,16 +1,26 @@
 const fs = require('fs')
 const CanvasApi = require('kth-canvas-api')
 require('dotenv').config()
-const canvasApi = new CanvasApi(apiUrl, apiKey)
+const canvasApi = new CanvasApi(process.env.CANVAS_API_URL,process.env.CANVAS_API_KEY )
 
-const csvLines = fs.readFileSync(oldProvisioningFile, 'utf8')
+const csvLines = fs.readFileSync('case-1-replace-addresses.csv', 'utf8')
     .toString()
     .split('\n')
 
 async function update() {
     for (const line of csvLines) {
+        const [canvasUserId,,ugEmail] = line.split(',')
+        if(canvasUserId === 'canvas_id') continue
+        console.log(canvasUserId, ugEmail)
 
-        process.exit()
+        const commChannels = await canvasApi.get(`users/${canvasUserId}/communication_channels`)
+        console.log(commChannels)
+        // THe user should only have one email channel, but double check anyway
+        if(commChannels.filter(channel => channel.type === 'email').length != 1){
+            console.error('The user has wrong number of email communication channels!', line)
+            process.exit()
+        }
+        
     }
 }
 update()
