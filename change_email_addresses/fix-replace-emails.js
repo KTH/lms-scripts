d28ad83893abfb633ebcbba4d7232e75542b3695
Emil Stenberg <emilsten@kth.se>
2019-01-24 13:15:57 +0100

    skip empty lines

--- change_email_addresses/fix-replace-emails.js
+++ change_email_addresses/fix-replace-emails.js
@@ -9,23 +9,28 @@ const csvLines = fs.readFileSync('case-1-replace-addresses.csv', 'utf8')
 
 async function update() {
     for (const line of csvLines) {
+        console.log('----------------')
         const [canvasUserId,,ugEmail] = line.split(',')
-        if(canvasUserId === 'canvas_id') continue
-        console.log(canvasUserId, ugEmail)
+
+
+        if(  canvasUserId === 'canvas_id') continue // skip headers
+        if(  !canvasUserId ) continue // Ignore empty lines
 
         const url = `users/${canvasUserId}/communication_channels`
         const commChannels = await canvasApi.get(url)
-        // THe user should only have one email channel, but double check anyway
         const emailCommChannels = commChannels.filter(channel => channel.type === 'email')
+        
+        // THe user should only have one email channel, but double check anyway
         if(emailCommChannels.length != 1){
             console.error('The user has wrong number of email communication channels!', line)
             process.exit()
         }
-        console.log('----------------')
         const [communication_channel] = emailCommChannels
 
         // Step one: delete the old email communication channel, since it's not possible to update them through the api
         await canvasApi.requestUrl( `${url}/${communication_channel.id}`, 'DELETE' )
+
+        // ... and then create a new one with the correct email address
         await canvasApi.requestUrl(
             `users/${canvasUserId}/communication_channels`,
             'POST',
@@ -39,9 +44,7 @@ async function update() {
 
         )
         console.log('done with updating the user with canvas id', canvasUserId)
-        process.exit()
-        //await canvasApi.requestUrl(`users/${canvasUserId}/communication_channels/`)
-
     }
+    console.log('done :)')
 }
 update()
