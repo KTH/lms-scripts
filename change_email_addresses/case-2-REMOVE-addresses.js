b5b74041060aa2c67b1e36ae5b6685d6ef199677
Emil Stenberg <emilsten@kth.se>
2019-01-25 10:22:24 +0100

    print all users that dont have incorrect emails. These should be checked manually

--- change_email_addresses/case-2-REMOVE-addresses.js
+++ change_email_addresses/case-2-REMOVE-addresses.js
@@ -3,15 +3,14 @@ const CanvasApi = require('kth-canvas-api')
 require('dotenv').config()
 const canvasApi = new CanvasApi(process.env.CANVAS_API_URL,process.env.CANVAS_API_KEY )
 
-const csvLines = fs.readFileSync('case-1-replace-addresses.csv', 'utf8')
+const csvLines = fs.readFileSync('case-2-REMOVE-addresses.csv', 'utf8')
     .toString()
     .split('\n')
 
 async function update() {
     for (const line of csvLines) {
-        console.log('----------------')
-        const [canvasUserId,,ugEmail] = line.split(',')
-
+        console.log('----------------', line)
+        const [canvasUserId,sisUserId,removeAddress] = line.split(',')
 
         if(  canvasUserId === 'canvas_id') continue // skip headers
         if(  !canvasUserId ) continue // Ignore empty lines
@@ -19,31 +18,19 @@ async function update() {
         const url = `users/${canvasUserId}/communication_channels`
         const commChannels = await canvasApi.get(url)
         const emailCommChannels = commChannels.filter(channel => channel.type === 'email')
-        
-        // THe user should only have one email channel, but double check anyway
-        if(emailCommChannels.length != 1){
-            console.error('The user has wrong number of email communication channels!', line)
-            process.exit()
-        }
-        const [communication_channel] = emailCommChannels
 
-        // Step one: delete the old email communication channel, since it's not possible to update them through the api
-        await canvasApi.requestUrl( `${url}/${communication_channel.id}`, 'DELETE' )
-
-        // ... and then create a new one with the correct email address
-        await canvasApi.requestUrl(
-            url,
-            'POST',
-            {
-                communication_channel:{
-                    type: 'email',
-                    address: ugEmail 
-                }
-                ,skip_confirmation:true
+        // The user should have more then one email channel, but double check anyway
+        if(emailCommChannels.length <= 1){
+            console.error('The user has wrong number of email communication channels!', emailCommChannels.length)
+        } else {
+            const removeChannel = emailCommChannels.find(channel => channel.address === removeAddress)
+            if(!removeChannel){
+                console.error('Could not find the incorrect email adress. Skipping this user!')
+            }else{
+                console.log('About to remove the communication channel',removeChannel)
+                await canvasApi.requestUrl( `${url}/${removeChannel.id}`, 'DELETE' )
             }
-
-        )
-        console.log('done with updating the user with canvas id', canvasUserId)
+        }
     }
     console.log('done :)')
 }
