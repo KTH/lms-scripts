70a59495703fa2921c9c56d3f5a3ecc47c63fc93
Emil Stenberg <emilsten@kth.se>
2019-01-31 10:51:12 +0100

    replace the incorrect email address with the correct address from ug

--- change_email_addresses/case-3-MAYBE-NOT-CORRECT-a:replace.js
+++ change_email_addresses/case-3-MAYBE-NOT-CORRECT-a:replace.js
@@ -10,7 +10,7 @@ const csvLines = fs.readFileSync('case-3-MAYBE-NOT-CORRECT-a:replace.csv', 'utf8
 async function update() {
     for (const line of csvLines) {
         console.log('----------------', line)
-        const [canvasUserId,sis_user_id,canvasEmail] = line.split(',')
+        const [canvasUserId,sis_user_id,canvasEmail,status,ugEmail,action,action_comment] = line.split(',')
 
         if(  canvasUserId === 'canvas_id') continue // skip headers
         if(  !canvasUserId ) continue // Ignore empty lines
@@ -20,15 +20,21 @@ async function update() {
         const [emailCommChannel ]= commChannels.filter(
             channel => channel.type === 'email' && 
             channel.address === canvasEmail)
-        
+
+       if(!emailCommChannel){
+            console.log('no incorrect email found, skipping')
+           continue
+       } 
         // THe user should only have one email channel, but double check anyway
         //const [communication_channel] = emailCommChannels
         console.log('emailCommChannel to replace: ',emailCommChannel)
-        // Step one: delete the old email communication channel, since it's not possible to update them through the api
-        /*await canvasApi.requestUrl( `${url}/${communication_channel.id}`, 'DELETE' )
 
+        console.log('replace with the email from ug instead: ', ugEmail)
+        // Step one: delete the old email communication channel, since it's not possible to update them through the api
+        await canvasApi.requestUrl( `${url}/${emailCommChannel.id}`, 'DELETE' )
+        
         // ... and then create a new one with the correct email address
-        await canvasApi.requestUrl(
+         await canvasApi.requestUrl(
             url,
             'POST',
             {
@@ -39,7 +45,7 @@ async function update() {
                 ,skip_confirmation:true
             }
 
-        )*/
+        )
         console.log('done with updating the user with canvas id', canvasUserId)
     }
     console.log('done :)')
