681ffc95dc7d3e9ca363b55cc4d4f840cdfe47a6
Emil Stenberg <emilsten@kth.se>
2019-02-05 14:58:23 +0100

    added a script that looks after invalid email adresses

---
+++ change_email_addresses/check-email-aliases.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+const csvLines = fs.readFileSync('/home/emil/Downloads/All\ the\ communication\ channels\ in\ Canvas.csv', 'utf8')
+    .toString()
+    .split('\n')
+
+const csvLinesUg = fs.readFileSync('/tmp/all-ug-users.csv', 'utf8')
+    .toString()
+    .split('\n')
+
+const ugUsers = [] 
+csvLinesUg
+    .map(line => line.split(','))
+    .forEach(([ugKthId, ugUsername,ugEmail, ...aliases]) => ugUsers.push({ugKthId, ugUsername, ugEmail,aliases}))
+
+async function update() {
+    for (const line of csvLines) {
+        const [canvasUserId, kthId,canvasEmail] = line.split(',')
+        // Seach for this email in all of the aliases
+        const foundAlias = ugUsers.find(ugUser => 
+            ugUser.aliases.find(alias => 
+                alias === canvasEmail &&  ugKthId !== kthId))
+        if(foundAlias){
+            console.log(canvasEmail,foundAlias)
+        }
+        //process.exit()
+    }
+}
+update()
