66894a94e536a378adf2755fc66bdbbcbc416d8d
Carlos Saito <carlos@saito.style>
2020-03-06 13:53:11 +0100

    Download all exams from a student

--- download-exam/index.js
+++ download-exam/index.js
@@ -1,26 +1,22 @@
 const inquirer = require('inquirer')
 const got = require('got')
 const fs = require('fs')
+const os = require('os')
+const path = require('path')
 
-async function getCourseDetails () {
-  const { courseCode, termNumber } = await inquirer.prompt([
+async function getCourseCode () {
+  const { courseCode } = await inquirer.prompt([
     {
       type: 'input',
       name: 'courseCode',
       message: 'Write a course code (XX0000)',
-    },
-    {
-      type: 'input',
-      name: 'termNumber',
-      message: 'Write a year and term (1 = Spring / 2 = Fall). Example: 20191',
-      default: '20192'
     }
   ])
 
-  return { courseCode, termNumber }
+  return courseCode
 }
 
-async function getModules ({courseCode, termNumber}) {
+async function getModules (courseCode) {
   const { body: courseDetails } = await got(
     `https://api.kth.se/api/kopps/v2/course/${courseCode}/detailedinformation`,
     {
@@ -28,15 +24,12 @@ async function getModules ({courseCode, termNumber}) {
     }
   )
 
-  const examinationSets = Object.values(courseDetails.examinationSets)
-    .sort(
-      (a, b) =>
-        parseInt(a.startingTerm.term, 10) - parseInt(b.startingTerm.term, 10)
-    )
-    .filter(e => parseInt(e.startingTerm.term, 10) <= termNumber)
-
-  const examinationRounds =
-    examinationSets[examinationSets.length - 1].examinationRounds
+  const examinationRounds = []
+  for (const set of Object.values(courseDetails.examinationSets)) {
+    for (const e of set.examinationRounds) {
+      examinationRounds.push(e)
+    }
+  }
 
   return examinationRounds.map(round => ({
     id: round.ladokUID,
@@ -78,52 +71,77 @@ async function getExams (courseCode, examId) {
     .map(result => ({
       id: result.fileId,
       date: result.createDate,
-      firstName: result.documentIndiceses.find(i => i.index === 's_firstname').value,
-      lastName: result.documentIndiceses.find(i => i.index === 's_lastname').value
+      kthId: getValue(result, 's_uid'),
+      firstName: getValue(result, 's_firstname'),
+      lastName: getValue(result, 's_lastname')
     }))
 }
 
-async function chooseExam (exams) {
+function getValue (exam, key) {
+  const keyValue = exam.documentIndiceses.find(i => i.index === key)
+
+  return keyValue && keyValue.value
+}
+
+async function chooseStudent (exams) {
   console.log(`Got ${exams.length} exams`)
+
+  // Group by student
+  const students = new Map()
+
+  for (const exam of exams) {
+    if (students.has(exam.kthId)) {
+      students.set(exam.kthId, students.get(exam.kthId) + 1)
+    } else {
+      students.set(exam.kthId, 1)
+    }
+  }
+
   const { answer } = await inquirer.prompt([{
     type: 'list',
     name: 'answer',
-    message: 'Choose an exam',
-    choices: exams.map(e => ({
-      name: `${e.lastName}, ${e.firstName} (${e.date})`,
-      value: e.id,
-      short: e.id
+    message: 'Choose a student',
+    choices: Array.from(students, ([k, v]) => ({
+      name: `${k} (it has ${v} exams)`,
+      value: k,
     }))
   }])
 
-  return answer
+  return exams
+    .filter(exam => exam.kthId === answer)
+    .map(exam => exam.id)
 }
 
-async function saveExam (fileId) {
-  const url = `https://tentaapi.ug.kth.se/api/v2.0/windream/file/${fileId}/true`
-  console.log(`Getting ${url}`)
+async function saveExams (fileIds) {
+  const directory = fs.mkdtempSync(os.tmpdir())
+
+  for (const fileId of fileIds) {
+    const url = `https://tentaapi.ug.kth.se/api/v2.0/windream/file/${fileId}/true`
+    console.log(`Getting ${url}`)
 
 
-  const {body} = await got(url, {
-    json: true
-  })
+    const {body} = await got(url, {
+      json: true
+    })
 
-  console.log(`Saving file to "/tmp/${body.wdFile.fileName}"...`)
-  const download = Buffer.from(body.wdFile.fileAsBase64.toString('utf-8'), 'base64')
-  fs.writeFileSync(`/tmp/${body.wdFile.fileName}`, download)
+    const filePath = path.join(directory, body.wdFile.fileName)
+
+    console.log(`Saving file to "${filePath}"...`)
+    const download = Buffer.from(body.wdFile.fileAsBase64.toString('utf-8'), 'base64')
+
+    fs.writeFileSync(filePath, download)
+  }
 }
 
 async function start () {
-  const {courseCode, termNumber} = await getCourseDetails()
-  const examId = await getModules({courseCode, termNumber})
+  const courseCode = await getCourseCode()
+  const examId = await getModules(courseCode)
     .then(chooseModule)
 
   const exams = await getExams(courseCode, examId)
 
-  while (true) {
-    await chooseExam(exams)
-      .then(saveExam)
-  }
+  await chooseStudent(exams)
+    .then(saveExams)
 }
 
 start()
