cb7d4eb817b0539f157b874f7689778889211ce8
Carlos Saito <carlos@saito.style>
2022-06-07 15:30:41 +0200

    Format

--- canvas-id-update/src/actions/createCsvForIdChange.ts
+++ canvas-id-update/src/actions/createCsvForIdChange.ts
@@ -1,29 +1,32 @@
 import path from "node:path";
 import { getCourseRounds } from "./kopps";
-import { createFolder, createCsvSerializer, TERMS_TO_IMPORT, createSisCourseId } from "./utils";
-
-
-
-
-
+import {
+  createFolder,
+  createCsvSerializer,
+  TERMS_TO_IMPORT,
+  createSisCourseId,
+} from "./utils";
 
 export default async function run({ outpDir }) {
   const outpDirPath = path.resolve(process.cwd(), outpDir);
   createFolder(outpDirPath);
-  
+
   const courseCsv = createCsvSerializer(`${outpDirPath}/courseChangeSisId.csv`);
-  const sectionCsv = createCsvSerializer(`${outpDirPath}/sectionChangeSisId.csv`);
-  const skippedCsv = createCsvSerializer(`${outpDirPath}/skippedChangeSisId.csv`);
+  const sectionCsv = createCsvSerializer(
+    `${outpDirPath}/sectionChangeSisId.csv`
+  );
+  const skippedCsv = createCsvSerializer(
+    `${outpDirPath}/skippedChangeSisId.csv`
+  );
 
   for (const term of TERMS_TO_IMPORT) {
     const courseRounds = await getCourseRounds(term);
     for (const row of courseRounds) {
-
       if (!row.ladokUid) {
         const outpRow = {
           ...row,
-          sis_id: createSisCourseId(row)
-        }
+          sis_id: createSisCourseId(row),
+        };
         skippedCsv.write(outpRow);
         continue;
       }
@@ -31,17 +34,17 @@ export default async function run({ outpDir }) {
       const newRow = {
         old_id: createSisCourseId(row),
         new_id: row.ladokUid,
-        new_integration_id: "<delete>" // We want to remove the integration id from the object
-      }
+        new_integration_id: "<delete>", // We want to remove the integration id from the object
+      };
 
       courseCsv.write({
         ...newRow,
-        type: "course"
+        type: "course",
       });
-      
+
       sectionCsv.write({
         ...newRow,
-        type: "section"
+        type: "section",
       });
     }
   }
@@ -49,6 +52,4 @@ export default async function run({ outpDir }) {
   courseCsv.end();
   sectionCsv.end();
   skippedCsv.end();
-};
-
-
+}
