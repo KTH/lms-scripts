6f7b7b831fca82aa60934b3e168bed7384aab267
Emil Stenberg <esandin@gmail.com>
2024-01-09 15:54:10 +0100

    include tsconfig

--- import-exams-stats/index.ts
+++ import-exams-stats/index.ts
@@ -1,15 +1,37 @@
 require("dotenv").config();
 const Canvas = require("@kth/canvas-api").default;
+import * as csv from "fast-csv";
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
 
 const canvas = new Canvas(
   process.env.CANVAS_API_URL,
   process.env.CANVAS_API_KEY
 );
-async function start() {
-  console.log("Making a GET request to /accounts/1");
 
-  const { body } = await canvas.get("accounts/1");
-  console.log(body);
+function createCsvSerializer(name) {
+  const writer = fs.createWriteStream(name);
+  const serializer = csv.format({ headers: true });
+  serializer.pipe(writer);
+  return serializer;
+}
+
+async function start() {
+  const baseDir = fs.mkdtempSync(path.join(os.tmpdir(), "sync-"));
+  const dir = path.join(baseDir, "csv");
+  fs.mkdirSync(dir);
+  console.log(`Creating csv files in ${dir}`);
+  const resultCsv = createCsvSerializer(`${dir}/import-exams-stats.csv`);
+
+  const courses = canvas.listItems("accounts/1/courses");
+
+  // Now `courses` is an iterator that goes through every course
+  for await (const course of courses) {
+    console.log(course);
+    resultCsv.write(course);
+  }
+  resultCsv.end();
 }
 
 start();
