86c576ddd66c33792b44bb41d4d98bed50dfccf7
Emil Stenberg <esandin@gmail.com>
2024-01-10 08:32:59 +0100

    added assignment type

--- import-exams-stats/index.ts
+++ import-exams-stats/index.ts
@@ -5,6 +5,31 @@ import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
 
+type assignment = {
+  id: number;
+  due_at: string;
+  unlock_at?: string;
+  lock_at?: string;
+  created_at: string;
+  updated_at: string;
+  anonymous_grading: boolean;
+  graders_anonymous_to_graders: boolean;
+  course_id: number;
+  name: string;
+  has_submitted_submissions: boolean;
+  graded_submissions_exist: false;
+  is_quiz_assignment: false;
+  workflow_state: string;
+  muted: boolean;
+  integration_id: null;
+  integration_data: any;
+  published: boolean;
+  unpublishable: boolean;
+  post_manually: boolean;
+  anonymize_students: boolean;
+  require_lockdown_browser: boolean;
+};
+
 const canvas = new Canvas(
   process.env.CANVAS_API_URL,
   process.env.CANVAS_API_KEY
@@ -24,21 +49,23 @@ async function start() {
   console.log(`Creating csv files in ${dir}`);
   const resultCsv = createCsvSerializer(`${dir}/import-exams-stats.csv`);
 
-  const courses = canvas.listItems("accounts/1/courses");
+  // TODO: go from root account here
+  const courses = canvas.listItems("accounts/110/courses");
 
-  // Now `courses` is an iterator that goes through every course
   for await (const course of courses) {
-    const assignments = canvas.listItems(`courses/${course.id}/assignments`);
-    for await (const assignment of assignments) {
-      if (assignment.integration_data?.ladokId) {
-        console.log(
-          `Assignment is created by import exams: ${assignment.name}`
-        );
-      } else {
-        process.stdout.write(".");
-      }
+    const assignments = await canvas
+      .listItems(`courses/${course.id}/assignments`)
+      .toArray();
+    const examAssignment = assignments.find((a) => a.integration_data?.ladokId);
+
+    if (examAssignment) {
+      console.log("is exam assignment", examAssignment);
+    } else {
+      process.stdout.write(".");
     }
     // resultCsv.write(course);
+
+    // console.log(`${course.name}`);
   }
   resultCsv.end();
 }
