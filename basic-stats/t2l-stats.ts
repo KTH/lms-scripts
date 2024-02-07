9a5624f7dba94802fee594573f516bbad50a9ad8
Emil Stenberg <esandin@gmail.com>
2024-02-07 21:26:56 +0100

    replace  - Imported course rounds

--- basic-stats/t2l-stats.ts
+++ basic-stats/t2l-stats.ts
@@ -43,21 +43,43 @@ async function start() {
   ];
 
   // TODO: using slice to subset the data
-  for await (const doc of docs.slice(0, 4)) {
+  for await (const doc of docs) {
+    // for await (const doc of docs.slice(0, 4)) {
     const { body: canvasCourse } = await canvas.get(
       `courses/${doc.parameters?.courseId}`
     );
     const { body: account } = await canvas.get(
       `accounts/${canvasCourse.account_id}`
     );
-    const accountName = account.name.replaceAll(" - Examinations", "");
-    const dateString = doc._id.getTimestamp().toLocaleDateString("Sv");
+    const accountName = account.name
+      .replaceAll(" - Examinations", "")
+      .replaceAll(" - Imported course rounds", "");
+    // console.log(doc);
+    let dateString;
+    try {
+      dateString = doc._id.getTimestamp().toLocaleDateString("Sv");
+    } catch (e) {
+      // Our custom id:s are in date format already
+      dateString = doc._id;
+    }
+    // console.log(doc);
 
     const result = {
-      account_name: accountName,
+      school: accountName,
       created_at: `${dateString.substring(0, 7)}`,
+      error: doc.summary?.error,
+      success: doc.summary?.success,
+      type: doc.parameters?.destination.aktivitetstillfalle
+        ? "examinationsrum"
+        : "kursrum",
     };
-    console.log(result);
+    process.stdout.write(".");
+    await new Promise<void>((resolve, reject) => {
+      resultCsv.write(result, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
   }
 
   resultCsv.end();
