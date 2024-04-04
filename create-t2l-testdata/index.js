99da2d4377e27fb47c25ebd8fdff320ac110fffe
Emil <esandin@gmail.com>
2024-04-04 15:21:35 +0200

    wip: getting adding users to work

--- create-t2l-testdata/index.js
+++ create-t2l-testdata/index.js
@@ -18,7 +18,8 @@ const ladokGot = got.extend({
 });
 
 async function start() {
-  await createAktivitetstillfalle();
+  // await createAktivitetstillfalle();
+  await addStudents();
 }
 
 async function createAktivitetstillfalle() {
@@ -49,7 +50,6 @@ async function createAktivitetstillfalle() {
     },
   });
   console.log(JSON.stringify(body, null, 2));
-  addStudents(body.Uid);
 }
 
 async function addStudents(uid) {
@@ -57,11 +57,123 @@ async function addStudents(uid) {
 
   await ladokGot.post(
     // Internal probably means that it isn't the same url, or params, when using the api
-    `resultat/aktivitetstillfallesmojlighet/studieresultat/7ed0b2b8-74d0-11ee-bf24-29cb18164efb`,
-    // `resultat/aktivitetstillfallesmojlighet/studieresultat/42ffc967-7698-11ee-bf24-29cb18164efb/skapaochanmal`,
+    // `resultat/aktivitetstillfallesmojlighet/studieresultat/7ed0b2b8-74d0-11ee-bf24-29cb18164efb`,
+    `resultat/aktivitetstillfallesmojlighet/studieresultat/f489fcb4-2c55-11ee-998a-6a4f18c04b6e/skapaochanmal`,
     {
-      body: {
+      _body: {
         AktivitetstillfalleUID: "f1d989ae-f1c9-11ee-835e-1693d1304552",
+      },
+      body: {
+        AktivitetstillfalleUID: "11111111-2222-0000-0000-000000000000",
+        AktivitetstillfallesalternativUID:
+          "11111111-2222-0000-0000-000000000000",
+        Anmalan: {
+          Anmalningsmeddelande: "Hej, jag behöver extra luft på tentan!",
+          Anmalningstidpunkt: "2012-01-01T14:00:00",
+          LarosateID: 96,
+          disabledLink: [
+            {
+              rel: "http://schemas.ladok.se",
+              method: "GET",
+              reasons: [
+                {
+                  sv: "Anledning",
+                  en: "Reason",
+                },
+              ],
+            },
+          ],
+          link: [
+            {
+              method: "POST",
+              uri: "https://api.mit.ladok.se:443/test",
+              mediaType: "application/vnd.ladok+xml",
+              rel: "http://schemas.ladok.se",
+            },
+          ],
+        },
+        Anonymiseringskod: "",
+        KurstillfalleStudentenArRegistreradPa:
+          "11111111-2222-0000-0000-000000000000",
+        LarosateID: 96,
+        Rapporteringskontext: {
+          KurstillfalleUID: "11111111-2222-0000-0000-000000000000",
+          LarosateID: 96,
+          UtbildningsinstansUID: "11111111-2222-0000-0000-000000000000",
+          disabledLink: [
+            {
+              rel: "http://schemas.ladok.se",
+              method: "GET",
+              reasons: [
+                {
+                  sv: "Anledning",
+                  en: "Reason",
+                },
+              ],
+            },
+          ],
+          link: [
+            {
+              method: "POST",
+              uri: "https://api.mit.ladok.se:443/test",
+              mediaType: "application/vnd.ladok+xml",
+              rel: "http://schemas.ladok.se",
+            },
+          ],
+        },
+        SenastAndradAv: "eva@ladok3.ladok.se",
+        SenastSparad: "2012-01-11T12:45:45",
+        Student: {
+          Efternamn: "Svensson",
+          Fornamn: "Sven",
+          LarosateID: 96,
+          Personnummer: "199611052383",
+          SenastAndradAv: "eva@ladok3.ladok.se",
+          SenastSparad: "2012-01-11T12:45:45",
+          Uid: "11111111-2222-0000-0000-000000000000",
+          disabledLink: [
+            {
+              rel: "http://schemas.ladok.se",
+              method: "GET",
+              reasons: [
+                {
+                  sv: "Anledning",
+                  en: "Reason",
+                },
+              ],
+            },
+          ],
+          link: [
+            {
+              method: "POST",
+              uri: "https://api.mit.ladok.se:443/test",
+              mediaType: "application/vnd.ladok+xml",
+              rel: "http://schemas.ladok.se",
+            },
+          ],
+        },
+        StudentUID: "11111111-2222-0000-0000-000000000000",
+        Uid: "11111111-2222-0000-0000-000000000000",
+        disabledLink: [
+          {
+            rel: "http://schemas.ladok.se",
+            method: "GET",
+            reasons: [
+              {
+                sv: "Anledning",
+                en: "Reason",
+              },
+            ],
+          },
+        ],
+        link: [
+          {
+            method: "POST",
+            uri: "https://api.mit.ladok.se:443/test",
+            mediaType: "application/vnd.ladok+xml",
+            rel: "http://schemas.ladok.se",
+          },
+        ],
       },
       headers: {
         Accept: "application/json",
