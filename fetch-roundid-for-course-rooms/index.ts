b3be2a76dcee0b46922ffa69dcef38ddd269bfd9
Emil Stenberg <esandin@gmail.com>
2024-08-07 20:48:54 +0200

    fetch course rounds for every section in the course rooms

--- fetch-roundid-for-course-rooms/index.ts
+++ fetch-roundid-for-course-rooms/index.ts
@@ -2,6 +2,13 @@ import fs from "node:fs";
 import { parse } from "csv-parse";
 import { request } from "undici";
 
+require("dotenv").config();
+const canvasApiUrl = process.env.CANVAS_API_URL;
+const canvasApiToken = process.env.CANVAS_API_TOKEN;
+
+import CanvasApi from "@kth/canvas-api";
+const canvas = new CanvasApi(canvasApiUrl as string, canvasApiToken as string);
+
 async function run() {
   // const records = [];
   const parser = fs.createReadStream(`course-rooms.csv`).pipe(
@@ -13,58 +20,62 @@ async function run() {
   const courseRounds: any[] = [];
   for await (const record of parser) {
     // Work with each record
-    const [
-      canvas_course_id,
-      course_id,
-      integration_id,
-      short_name,
-      long_name,
-      canvas_account_id,
-      account_id,
-      canvas_term_id,
-      term_id,
-      status,
-      start_date,
-      end_date,
-      course_format,
-      canvas_blueprint_course_id,
-      blueprint_course_id,
-      created_by_sis,
-    ] = record;
+    const [canvas_course_id, course_id] = record;
 
     // First row is header
-    if (i !== 0) continue;
-    if (!course_id) continue; // Skip course rooms without sis_id
-    // Find the course round in Kopps, to get the roundId
-    const courseCode = short_name.split(" ")[0];
-    const koppsUrl = `https://api.kth.se/api/kopps/v2/course/${courseCode}/detailedinformation`;
-
-    console.log(koppsUrl, " for course with id: ", canvas_course_id);
-
-    try {
-      // TODO: how to handle samläsning? Loop through all sections for id:s instead of courses?
-      const { statusCode, headers, trailers, body } = await request(koppsUrl);
-      const detailedinformation: any = await body.json();
-      const { roundInfos } = detailedinformation;
-
-      const matchingRound = roundInfos.find(
-        (roundInfo: any) => roundInfo.round.ladokUID === course_id
-      );
-
-      // console.log("matchingRound", matchingRound);
-      // TODO: Same format as course/offerings?
-      courseRounds.push({
-        course_id,
-        short_name,
-        status,
-        courseCode,
-        round: matchingRound.round,
-      });
-      // break;
-    } catch (error) {
-      console.error(`Error fetching course from Kopps ${koppsUrl}`, error);
-    }
     i++;
+    if (i <= 1) continue;
+    if (!course_id) continue; // Skip course rooms without sis_id
+    //
+    // Some course rooms can have multiple sections, one per course round
+    const { body: sections } = await canvas.get(
+      `courses/${canvas_course_id}/sections`
+    );
+
+    /** Example section
+     * {
+    id: 67257,
+    course_id: 50374,
+    name: 'SF1624 HT24 (CINTE1)',
+    start_at: null,
+    end_at: null,
+    created_at: '2024-02-02T03:36:23Z',
+    restrict_enrollments_to_section_dates: false,
+    nonxlist_course_id: 50378,
+    sis_section_id: 'a8611bb8-9a88-11ee-bfe5-0721067a4fbf',
+    sis_course_id: '0772a941-98c8-11ee-888d-8a62d8d3440a',
+    integration_id: null,
+    sis_import_id: 1745682
+  },
+     */
+    for await (const section of sections as any[]) {
+      const courseCode = section.name.split(" ")[0];
+      const koppsUrl = `https://api.kth.se/api/kopps/v2/course/${courseCode}/detailedinformation`;
+      console.log(koppsUrl, " for course with id: ");
+
+      try {
+        const { body } = await request(koppsUrl);
+        const detailedinformation: any = await body.json();
+        const { roundInfos } = detailedinformation;
+
+        const matchingRound = roundInfos.find(
+          (roundInfo: any) =>
+            roundInfo.round.ladokUID === section.sis_section_id
+        );
+
+        // console.log("matchingRound", matchingRound);
+        // TODO: Same format as course/offerings?
+        courseRounds.push({
+          ladokUID: section.sis_section_id,
+          name: section.name,
+          courseCode,
+          round: matchingRound.round,
+        });
+        // break;
+      } catch (error) {
+        console.error(`Error fetching course from Kopps ${koppsUrl}`, error);
+      }
+    }
   }
   fs.writeFileSync(
     "course-rooms-with-rounds.json",
