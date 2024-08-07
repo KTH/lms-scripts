08857f3140b7b313c74d880dc49caae36981c04a
Emil Stenberg <esandin@gmail.com>
2024-08-07 19:46:59 +0200

    added simple error handling

--- fetch-roundid-for-course-rooms/index.ts
+++ fetch-roundid-for-course-rooms/index.ts
@@ -38,25 +38,30 @@ async function run() {
       const courseCode = short_name.split(" ")[0];
       const koppsUrl = `https://api.kth.se/api/kopps/v2/course/${courseCode}/detailedinformation`;
 
-      console.log(koppsUrl);
-      const { statusCode, headers, trailers, body } = await request(koppsUrl);
-      const detailedinformation: any = await body.json();
-      const { roundInfos } = detailedinformation;
+      console.log(koppsUrl, " for course with id: ", canvas_course_id);
 
-      const matchingRound = roundInfos.find(
-        (roundInfo: any) => roundInfo.round.ladokUID === course_id
-      );
+      try {
+        const { statusCode, headers, trailers, body } = await request(koppsUrl);
+        const detailedinformation: any = await body.json();
+        const { roundInfos } = detailedinformation;
 
-      // console.log("matchingRound", matchingRound);
-      // TODO: Same format as course/offerings?
-      courseRounds.push({
-        course_id,
-        short_name,
-        status,
-        courseCode,
-        round: matchingRound.round,
-      });
-      // break;
+        const matchingRound = roundInfos.find(
+          (roundInfo: any) => roundInfo.round.ladokUID === course_id
+        );
+
+        // console.log("matchingRound", matchingRound);
+        // TODO: Same format as course/offerings?
+        courseRounds.push({
+          course_id,
+          short_name,
+          status,
+          courseCode,
+          round: matchingRound.round,
+        });
+        // break;
+      } catch (error) {
+        console.error(`Error fetching course from Kopps ${koppsUrl}`, error);
+      }
     }
     i++;
   }
